fb169b2672d556380b41282e8f0c3925c356095f
Sean Kirmani <sean@kirmani.io>
2016-01-22 11:19:47 -0600

    removed tabs (spaces rule)

--- MultiplayerJava/app/src/main/java/com/projecttango/experiments/multi/MainActivity.java
+++ MultiplayerJava/app/src/main/java/com/projecttango/experiments/multi/MainActivity.java
@@ -49,18 +49,18 @@ import java.util.ArrayList;
  * the device camera.
  */
 public class MainActivity extends Activity {
-	private static final String TAG = "MultiplayerTango";
-	private TangoCameraPreview tangoCameraPreview;
-	private Tango mTango;
-	private boolean mIsConnected;
+    private static final String TAG = "MultiplayerTango";
+    private TangoCameraPreview tangoCameraPreview;
+    private Tango mTango;
+    private boolean mIsConnected;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		tangoCameraPreview = new TangoCameraPreview(this);
-		mTango = new Tango(this);
-		setContentView(tangoCameraPreview);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        tangoCameraPreview = new TangoCameraPreview(this);
+        mTango = new Tango(this);
+        setContentView(tangoCameraPreview);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -69,70 +69,70 @@ public class MainActivity extends Activity {
         return true;
     }
 
-	// Camera Preview
-	private void startCameraPreview() {
-	    // Connect to color camera
-		tangoCameraPreview.connectToTangoCamera(mTango,
-				TangoCameraIntrinsics.TANGO_CAMERA_COLOR);
-		// Use default configuration for Tango Service.
-		TangoConfig config = mTango.getConfig(TangoConfig.CONFIG_TYPE_DEFAULT);
-		config.putBoolean(TangoConfig.KEY_BOOLEAN_LEARNINGMODE, true);
-		mTango.connect(config);
-		mIsConnected = true;
+    // Camera Preview
+    private void startCameraPreview() {
+        // Connect to color camera
+        tangoCameraPreview.connectToTangoCamera(mTango,
+                TangoCameraIntrinsics.TANGO_CAMERA_COLOR);
+        // Use default configuration for Tango Service.
+        TangoConfig config = mTango.getConfig(TangoConfig.CONFIG_TYPE_DEFAULT);
+        config.putBoolean(TangoConfig.KEY_BOOLEAN_LEARNINGMODE, true);
+        mTango.connect(config);
+        mIsConnected = true;
 
-		final ArrayList<TangoCoordinateFramePair> framePairs = new ArrayList<TangoCoordinateFramePair>();
-		framePairs.add(new TangoCoordinateFramePair(
-				TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
-				TangoPoseData.COORDINATE_FRAME_DEVICE));
-		mTango.connectListener(framePairs, new OnTangoUpdateListener() {
-			@Override
-			public void onPoseAvailable(TangoPoseData pose) {
-				Log.d(TAG, pose.toString());
-			}
+        final ArrayList<TangoCoordinateFramePair> framePairs = new ArrayList<TangoCoordinateFramePair>();
+        framePairs.add(new TangoCoordinateFramePair(
+                TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
+                TangoPoseData.COORDINATE_FRAME_DEVICE));
+        mTango.connectListener(framePairs, new OnTangoUpdateListener() {
+            @Override
+            public void onPoseAvailable(TangoPoseData pose) {
+                Log.d(TAG, pose.toString());
+            }
 
-			@Override
-			public void onFrameAvailable(int cameraId) {
-			    // Check if the frame available is for the camera we want and
-			    // update its frame on the camera preview.
-				if (cameraId == TangoCameraIntrinsics.TANGO_CAMERA_COLOR) {
-					tangoCameraPreview.onFrameAvailable();
-				}
-			}
+            @Override
+            public void onFrameAvailable(int cameraId) {
+                // Check if the frame available is for the camera we want and
+                // update its frame on the camera preview.
+                if (cameraId == TangoCameraIntrinsics.TANGO_CAMERA_COLOR) {
+                    tangoCameraPreview.onFrameAvailable();
+                }
+            }
 
-			@Override
-			public void onXyzIjAvailable(TangoXyzIjData xyzIj) {
-				// We are not using OnPoseAvailable for this app
-			}
+            @Override
+            public void onXyzIjAvailable(TangoXyzIjData xyzIj) {
+                // We are not using OnPoseAvailable for this app
+            }
 
-			@Override
-			public void onTangoEvent(TangoEvent event) {
-				// We are not using OnPoseAvailable for this app
-			}
-		});
-	}
+            @Override
+            public void onTangoEvent(TangoEvent event) {
+                // We are not using OnPoseAvailable for this app
+            }
+        });
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		if(mIsConnected) {
-			mTango.disconnect();
-			tangoCameraPreview.disconnectFromTangoCamera();
-			mIsConnected = false;
-		}
-	}
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mIsConnected) {
+            mTango.disconnect();
+            tangoCameraPreview.disconnectFromTangoCamera();
+            mIsConnected = false;
+        }
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		try {
-			if (!mIsConnected) {
-				startCameraPreview();
-			}
-		} catch (TangoOutOfDateException e) {
-			Toast.makeText(getApplicationContext(), R.string.TangoOutOfDateException,
-					Toast.LENGTH_SHORT).show();
-		} catch (TangoErrorException e) {
-			Toast.makeText(getApplicationContext(), R.string.TangoError, Toast.LENGTH_SHORT).show();
-		}
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            if (!mIsConnected) {
+                startCameraPreview();
+            }
+        } catch (TangoOutOfDateException e) {
+            Toast.makeText(getApplicationContext(), R.string.TangoOutOfDateException,
+                    Toast.LENGTH_SHORT).show();
+        } catch (TangoErrorException e) {
+            Toast.makeText(getApplicationContext(), R.string.TangoError, Toast.LENGTH_SHORT).show();
+        }
+    }
 }

0c489ebfa8cf531aef6d31e8602b629c4f27f4f1
Sean Kirmani <sean@kirmani.io>
2016-02-01 20:08:51 -0600

    Added Sandbox

---
+++ SandboxJava/app/src/main/java/com/projecttango/experiments/sandbox/AugmentedRealityRenderer.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.projecttango.experiments.sandbox;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.MotionEvent;
+
+import com.google.atap.tangoservice.TangoPoseData;
+
+import org.rajawali3d.Object3D;
+import org.rajawali3d.lights.DirectionalLight;
+import org.rajawali3d.materials.Material;
+import org.rajawali3d.materials.methods.DiffuseMethod;
+import org.rajawali3d.materials.textures.ATexture;
+import org.rajawali3d.materials.textures.Texture;
+import org.rajawali3d.math.vector.Vector3;
+import org.rajawali3d.primitives.Cube;
+import org.rajawali3d.util.RayPicker;
+import org.rajawali3d.util.OnObjectPickedListener;
+
+import com.projecttango.rajawali.DeviceExtrinsics;
+import com.projecttango.rajawali.Pose;
+import com.projecttango.rajawali.ScenePoseCalculator;
+import com.projecttango.rajawali.ar.TangoRajawaliRenderer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Very simple example augmented reality renderer which displays a cube fixed in place.
+ * Whenever the user clicks on the screen, the cube is placed flush with the surface detected
+ * with the depth camera in the position clicked.
+ *
+ * This follows the same development model than any regular Rajawali application
+ * with the following peculiarities:
+ * - It extends <code>TangoRajawaliArRenderer</code>.
+ * - It calls <code>super.initScene()</code> in the initialization.
+ * - When an updated pose for the object is obtained after a user click, the object pose is updated
+ *   in the render loop
+ * - The associated AugmentedRealityActivity is taking care of updating the camera pose to match
+ *   the displayed RGB camera texture and produce the AR effect through a Scene Frame Callback
+ *   (@see AugmentedRealityActivity)
+ */
+public class AugmentedRealityRenderer extends TangoRajawaliRenderer
+        implements OnObjectPickedListener {
+    private static final String TAG = "AugmentedRealityRenderer";
+    private static final float CUBE_SIDE_LENGTH = 0.5f;
+
+    private RayPicker mPicker;
+    private boolean mReadyToAddObject = false;
+    private boolean mReadyToPickObject = false;
+
+    public AugmentedRealityRenderer(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void initScene() {
+        // Remember to call super.initScene() to allow TangoRajawaliArRenderer
+        // to be set-up.
+        super.initScene();
+
+        // Add a directional light in an arbitrary direction.
+        DirectionalLight light = new DirectionalLight(1, 0.2, -1);
+        light.setColor(1, 1, 1);
+        light.setPower(0.8f);
+        light.setPosition(3, 2, 4);
+        getCurrentScene().addLight(light);
+
+        mPicker = new RayPicker(this);
+        mPicker.setOnObjectPickedListener(this);
+    }
+
+    @Override
+    protected void onRender(long elapsedRealTime, double deltaTime) {
+        super.onRender(elapsedRealTime, deltaTime);
+        if (mReadyToPickObject) {
+            dragObject();
+            mReadyToPickObject = false;
+        } else if (mReadyToAddObject) {
+            addObject();
+            mReadyToAddObject = false;
+        }
+    }
+
+    private void addObject() {
+        Object3D object = new Cube(CUBE_SIDE_LENGTH);
+        Material material = new Material();
+        material.setColor(0xff009900);
+        try {
+            Texture t = new Texture("instructions", R.drawable.instructions);
+            material.addTexture(t);
+        } catch (ATexture.TextureException e) {
+            e.printStackTrace();
+        }
+        material.setColorInfluence(0.1f);
+        material.enableLighting(true);
+        material.setDiffuseMethod(new DiffuseMethod.Lambert());
+        object.setMaterial(material);
+        object.setPosition(getCurrentCamera().getPosition());
+        object.setOrientation(getCurrentCamera().getOrientation());
+        object.moveForward(-1.0f);
+        getCurrentScene().addChild(object);
+    }
+
+    private void dragObject() {}
+
+    /**
+     * Update the scene camera based on the provided pose in Tango start of service frame.
+     * The device pose should match the pose of the device at the time the last rendered RGB
+     * frame, which can be retrieved with this.getTimestamp();
+     *
+     * NOTE: This must be called from the OpenGL render thread - it is not thread safe.
+     */
+    public void updateRenderCameraPose(TangoPoseData devicePose, DeviceExtrinsics extrinsics) {
+        Pose cameraPose = ScenePoseCalculator.toOpenGlCameraPose(devicePose, extrinsics);
+        getCurrentCamera().setRotation(cameraPose.getOrientation());
+        getCurrentCamera().setPosition(cameraPose.getPosition());
+    }
+
+    @Override
+    public void onOffsetsChanged(float xOffset, float yOffset,
+                                 float xOffsetStep, float yOffsetStep,
+                                 int xPixelOffset, int yPixelOffset) {
+    }
+
+    @Override
+    public void onTouchEvent(MotionEvent event) {
+        mReadyToAddObject = true;
+    }
+
+    @Override
+    public void onObjectPicked(Object3D object) {
+        mReadyToPickObject = true;
+    }
+
+    public void getObjectAt(float x, float y) {
+        mPicker.getObjectAt(x, y);
+    }
+}
